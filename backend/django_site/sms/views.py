a1f88d13b5529407db53521ed223b3994a3b0cf4
zhaojunx <zhaojunx@student.unimelb.edu.au>
2022-08-29 20:49:45 +1000

    delete useless code

--- backend/django_site/sms/views.py
+++ backend/django_site/sms/views.py
@@ -15,25 +15,6 @@ import tweepy
 class QuerySMS(APIView):
     @staticmethod
     def get(request):
-
-        # req = json.loads(request.body.decode().replace("'", "\""))
-        # tid = req.get('id')
-        # client = tweepy.Client(bearer_token=tw_cbd_credentials.bearer_token)
-        # twitter_id = client.get_user(username=tid).data.id
-        # print(twitter_id)
-
-        # req = json.loads(request.body.decode().replace("'", "\""))
-        # uid = req.get('id')
-        # twitter_id = models.TbClient.objects.get(uid=uid).twitter_id
-        
-        # tc.retrieve_2weeks_tweets_manul(twitter_id)
-
-        # GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY','1')
-        # print(GOOGLE_API_KEY)
-        # gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
-        # point = (-37.77433551264112, 144.95981690785675 )
-        # place2 = gmaps.reverse_geocode(point)
-        # print(place2)
        
         return Response()
 
