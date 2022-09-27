640e536e5f4d06fa7d15cc0618d6d0959bd1b902
Marvin3Benz <1074965720@qq.com>
2022-09-27 17:49:21 +1000

    Added three tests

--- frontend/src/testing/Loginpage.test.tsx
+++ frontend/src/testing/Loginpage.test.tsx
@@ -1,25 +1,27 @@
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-import "@testing-library/jest-dom";
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
-import Loginpage from "../pages/Login/Loginpage";
-import { MemoryRouter } from "react-router-dom";
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import '@testing-library/jest-dom';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import Loginpage from '../pages/Login/Loginpage';
+import { MemoryRouter } from 'react-router-dom';
 import URL from '../constant/Endpoint';
 
 const fakeClinicianInfo = {
-  "refresh": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY2NDI0NDA0NSwiaWF0IjoxNjY0MTU3NjQ1LCJqdGkiOiJmNzAyN2Y5NjEyYmY0MTBmOTVmOTczODU2ZGFmMGFhZCIsInVzZXJfaWQiOjQ5fQ.YQkwHVnH5Ko49WwLiJDJnNvCqdqBrW0JQ5JBUCwogkE",
-  "access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY0MjQ0MDQ1LCJpYXQiOjE2NjQxNTc2NDUsImp0aSI6IjEyMmU2MzM0NTU1NTQ1NGQ4ZjcyOTVkYWQwZmIwNTFkIiwidXNlcl9pZCI6NDl9.afdEUW6fqMhZ_Qk-v_dimNSBcpwMuvqQmyPHc_SuoYM",
-  "user_info": {
-      "id": 49,
-      "username": "Tony",
-      "first_name": "Tony",
-      "last_name": "Furgeson",
-      "email": "tony@gmail.com",
-      "is_staff": 1
-  }
-}
+  refresh:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY2NDI0NDA0NSwiaWF0IjoxNjY0MTU3NjQ1LCJqdGkiOiJmNzAyN2Y5NjEyYmY0MTBmOTVmOTczODU2ZGFmMGFhZCIsInVzZXJfaWQiOjQ5fQ.YQkwHVnH5Ko49WwLiJDJnNvCqdqBrW0JQ5JBUCwogkE',
+  access:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY0MjQ0MDQ1LCJpYXQiOjE2NjQxNTc2NDUsImp0aSI6IjEyMmU2MzM0NTU1NTQ1NGQ4ZjcyOTVkYWQwZmIwNTFkIiwidXNlcl9pZCI6NDl9.afdEUW6fqMhZ_Qk-v_dimNSBcpwMuvqQmyPHc_SuoYM',
+  user_info: {
+    id: 49,
+    username: 'Tony',
+    first_name: 'Tony',
+    last_name: 'Furgeson',
+    email: 'tony@gmail.com',
+    is_staff: 1,
+  },
+};
 const server = setupServer(
-  rest.post( URL.BASE_URL +  "/login/",  (req, res, ctx) => {
+  rest.post(URL.BASE_URL + '/login/', (req, res, ctx) => {
     return res(ctx.json(fakeClinicianInfo));
   })
 );
@@ -29,8 +31,7 @@ afterEach(() => {
 });
 afterAll(() => server.close());
 
-test("allows the user to login successfully", async () => {
-  
+test('allows the user to login successfully', async () => {
   render(
     <MemoryRouter>
       <Loginpage />
@@ -39,14 +40,16 @@ test("allows the user to login successfully", async () => {
 
   // fill out the form
   fireEvent.change(screen.getByRole(/account/i), {
-    target: { value: "tony@gmail.com" },
+    target: { value: 'tony@gmail.com' },
   });
   fireEvent.change(screen.getByRole(/password/i), {
-    target: { value: "p@ssowrd2" },
+    target: { value: 'P@ssword2' },
   });
 
   fireEvent.click(screen.getByText(/Login/i));
-  await waitFor(() => expect(screen.getByRole('login')).toHaveTextContent(/Login/i))
-  await waitFor(()=>expect(screen.getByRole(/password/i)).toHaveTextContent(''))
-  await waitFor(()=>expect(window.sessionStorage.getItem("userInfo")).toEqual(JSON.stringify(fakeClinicianInfo)));
+  await waitFor(() => expect(screen.getByRole('login')).toHaveTextContent(/Login/i));
+  await waitFor(() => expect(screen.getByRole(/password/i)).toHaveTextContent(''));
+  await waitFor(() =>
+    expect(window.sessionStorage.getItem('userInfo')).toEqual(JSON.stringify(fakeClinicianInfo))
+  );
 });
